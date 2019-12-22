a9616874576cad0712f7c7745fb23fe11577c7ff
Ken Harris <kengruven@gmail.com>
2019-12-21 19:45:24 -0800

    Add empty Xcode project
    The .gitignore is (part of) Strukt's.

---
+++ Ichi Demo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Ichi Demo
+//
+//  Created by Ken Harris on 12/21/19.
+//  Copyright © 2019 Free Robot Collective. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Ichi Demo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Ichi Demo
+//
+//  Created by Ken Harris on 12/21/19.
+//  Copyright © 2019 Free Robot Collective. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
